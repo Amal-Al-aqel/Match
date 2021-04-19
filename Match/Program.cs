e73f5173da20b161d4eb912a37f29ddb500def79
your-name <your-email@example.com>
2021-04-19 04:14:08 +0300

    Add project files.

---
+++ Match/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        string s = "11299211";
+        string leftString = "";
+        string rightString = "";
+        int halfString = 0;
+        if (s.Length % 2 == 0)
+        {
+            halfString = s.Length / 2;
+            for (int i = 0; i < halfString; i++)
+            {
+                leftString += s[i];
+                rightString += s[s.Length-1-i];
+            }
+            Console.WriteLine(leftString);
+            Console.WriteLine(rightString);
+
+            if (rightString == leftString)
+            {
+                Console.WriteLine("True");
+            } else
+            {
+                Console.WriteLine("Flase");
+            }
+
+        }
+        else
+        {
+            Console.WriteLine("False");
+        }
+    }
+}
